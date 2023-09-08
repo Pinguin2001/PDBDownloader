6f45f593b46adcd576f8620f4dd0b5f6cc3d05fa
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2023-09-08 17:11:29 +0200

    Upload source

---
+++ PDBDownloader/PDBDownloadHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace PDBDownloader;
+
+public static class PDBDownloadHelper
+{
+    public static async Task<byte[]> DownloadPDBAsync(string pdburl)
+    {
+        HttpClient client = new();
+        byte[] bytes = await client.GetByteArrayAsync(new Uri(pdburl));
+        client.Dispose();
+        return bytes;
+    }
+}
---
+++ PDBDownloader/Program.cs
@@ -0,0 +1,34 @@
+﻿using PDBDownloader;
+using PE2PDBUrl;
+using System.Reflection;
+
+if (args.Length != 2)
+{
+    Console.WriteLine($"PDBDownloader {Assembly.GetExecutingAssembly().GetName().Version}");
+    Console.WriteLine("Downloads PDB files from msdl.microsoft.com");
+    Console.WriteLine("\nUsage: <PE File> <PDB output file>");
+    return;
+}
+
+string pefilepath = args[0];
+string pdbfilepath = args[1];
+if (!File.Exists(pefilepath))
+{
+    Console.WriteLine($"{pefilepath} does not exist");
+    return;
+}
+
+Console.WriteLine("Parsing PE file...");
+PEDebugInformation peDebugInformation = PEDebugInformation.GetDebugURLs(pefilepath);
+if (string.IsNullOrEmpty(peDebugInformation.PDBUrl))
+{
+    Console.WriteLine($"{pefilepath} cannot be parsed correctly to retrieve debug information.\nThe file might be corrupt.");
+    return;
+}
+
+Console.WriteLine($"Downloading pdb {peDebugInformation.PDBUrl}...");
+byte[] pdb = await PDBDownloadHelper.DownloadPDBAsync(peDebugInformation.PDBUrl);
+var pdbfile = File.Create(pdbfilepath);
+await pdbfile.WriteAsync(pdb);
+
+Console.WriteLine($"PDB saved to {pdbfilepath}");
